0db9c5e05a78f7acb7bff6b0e06f4bcad81f8d69
Ryan Wallace <rywall@gmail.com>
2011-04-18 10:31:51 -0700

    Don't use old style routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do |map|
+Rails.application.routes.draw do
   match 's3_assets/uploader' => 's3_assets#uploader'
   match 's3_assets/asset_uploaded' => 's3_assets#asset_uploaded', :as => 's3_asset_uploaded'
 end
