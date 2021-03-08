d621b7b05816b0d7ef9b5a018561ffd346652d26
Ryan Wallace <rywall@gmail.com>
2021-03-07 20:39:08 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  match 's3_assets/uploader' => 's3_assets#uploader'
-  match 's3_assets/asset_uploaded' => 's3_assets#asset_uploaded', :as => 's3_asset_uploaded'
+  match 's3_assets/uploader' => 's3_assets#uploader', via: [:get, :post]
+  match 's3_assets/asset_uploaded' => 's3_assets#asset_uploaded', :as => 's3_asset_uploaded', via: [:get, :post]
 end
